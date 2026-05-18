0aa4c9821314c1b7923c9c2912b1e6a7bac2139a
Felipe Rafael Bueno de Moura <felipemoura10k@gmail.com>
2026-05-17 23:49:58 -0300

    importação

---
+++ importação/importar_modulos.py
@@ -0,0 +1,7 @@
+import math
+resultado = math.sqrt(25)
+print(resultado)  # Imprime 5.0
+
+from math import sqrt
+resultado = sqrt(25)
+print(resultado)  # Imprime 5.0
---
+++ modulos/teste_modulo.py
@@ -0,0 +1,5 @@
+import meu_modulo
+
+meu_modulo.saudar("João")  # Imprime "Olá, João!"
+resultado = meu_modulo.calcular_soma(5, 3)
+print(resultado)  # Imprime 8
