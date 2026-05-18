9c32a1c47ba1ce30d75d9a6d16a03a8aa94c516f
Felipe Rafael Bueno de Moura <felipemoura10k@gmail.com>
2026-05-17 23:34:54 -0300

    exeções

---
+++ exeções/finally.py
@@ -0,0 +1,8 @@
+try:
+    # Código que pode gerar uma exceção
+    arquivo = open("arquivo.txt", "r")
+    # Realizar operações com o arquivo
+except FileNotFoundError:
+    print("Erro: Arquivo não encontrado")
+finally:
+    arquivo.close()  # Fechar o arquivo sempre, mesmo se ocorrer uma exceção
